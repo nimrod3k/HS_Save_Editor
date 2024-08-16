f630da6771bd7d7347d35f5dd5e52f1792a3a4aa
nimrod3k <col08020@byui.edu>
2024-08-16 13:04:50 -0400

    Added Door Locator

---
+++ Door_Locator/Door Locator/Door_Finder.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Door_Locator
+{
+    partial class Door_Finder
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.combo_saveFile = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // combo_saveFile
+            // 
+            this.combo_saveFile.FormattingEnabled = true;
+            this.combo_saveFile.Location = new System.Drawing.Point(73, 17);
+            this.combo_saveFile.Name = "combo_saveFile";
+            this.combo_saveFile.Size = new System.Drawing.Size(121, 23);
+            this.combo_saveFile.TabIndex = 0;
+            this.combo_saveFile.SelectedIndexChanged += new System.EventHandler(this.combo_saveFile_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Save File:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(212, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Door_Finder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 124);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.combo_saveFile);
+            this.Name = "Door_Finder";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ComboBox combo_saveFile;
+        private TextBox textBox1;
+        private Label label1;
+        private Button button1;
+    }
+}
---
+++ Door_Locator/Door Locator/Door_Finder.cs
@@ -0,0 +1,108 @@
+using Microsoft.VisualBasic.ApplicationServices;
+using HS_Tools;
+using System.Text.Json;
+
+namespace Door_Locator
+{
+    public partial class Door_Finder : Form
+    {
+        string _SavePath;
+        public Door_Finder()
+        {
+            string appData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            _SavePath = Path.Combine(appData, @"Hero's Spirit");
+            InitializeComponent();
+            if (Directory.Exists(_SavePath))
+            {
+                foreach (var file in Directory.GetFiles(_SavePath, "savedat?"))
+                {
+                    combo_saveFile.Items.Add(Path.GetFileNameWithoutExtension(file));
+                }
+            }
+            HS_Save_Tools.Initialize();
+        }
+
+        internal static string Unscramble(string text, int TotalSteps)
+        {
+            string text2 = text.Substring(10);
+            string text3 = text2.Substring(0, 4);
+            for (int i = 0; i <= (text2.Length - 4) / 8; i++)
+            {
+                string s = text2.Substring(Math.Min(text2.Length - 1, i * 8 + 4), Math.Min(8, text2.Length - (i * 8 + 4)));
+                text3 += HS_Save_Tools.Decode(s, TotalSteps + i);
+            }
+            text2 = text3;
+            text3 = "";
+            for (int j = 0; j <= text2.Length / 8; j++)
+            {
+                string s2 = text2.Substring(j * 8, Math.Min(8, text2.Length - j * 8));
+                text3 += HS_Save_Tools.Decode(s2, TotalSteps + j);
+            }
+            return text3;
+        }
+        internal static void Set(ref HSJsonData data, Vars item, byte val)
+        {
+            if ((int)item < data.values.Length)
+                data.values[(int)item] = HS_Save_Tools.ObfuscateValue(val);
+        }
+
+        internal static void getFile()
+        {
+            var lines = File.ReadLines(@"C:\Users\nimro\source\repos\HS_Old\HS_Save_Editor\Door_Locator\Door Locator\flags.txt");
+            List<String> writelines = new List<String>();
+            foreach (var line in lines)
+            {
+                string longvalue = line.Split(':')[1];
+                string[] values = line.Split('-');
+                string doortype = "";
+                if (values[0] == "sdoor" || values[1] == "sdoor" || values[2] == "sdoor")
+                    doortype = "s";
+                if (values[0] == "gdoor" || values[1] == "gdoor" || values[2] == "gdoor")
+                    doortype = "g";
+                if (doortype != "")
+                {
+                    writelines.Add(line.Split(':')[0] + ":" + doortype);
+                }
+            }
+            File.WriteAllLines(@"C:\Users\nimro\source\repos\HS_Old\HS_Save_Editor\Door_Locator\Door Locator\doors.txt", writelines);
+        }
+
+        private void combo_saveFile_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string savefile = Path.Combine(_SavePath, (string)combo_saveFile.SelectedItem);
+            HSJsonData data = new HSJsonData();
+            string text = File.ReadAllText(savefile);
+            int TotalSteps = int.Parse(text.Substring(0, 10));
+            string saveData = Unscramble(text, TotalSteps);
+            if (saveData != null)
+            {
+                data = JsonSerializer.Deserialize<HSJsonData>(saveData);
+                if (data != null && data.values != null)
+                {
+                    var lines = File.ReadLines(@"C:\Users\nimro\source\repos\HS_Old\HS_Save_Editor\Door_Locator\Door Locator\doors.txt");
+                    
+                    List<string> missing_doors = new List<string>();
+                    
+
+                    foreach (var line in lines)
+                    {
+                        string[] split_line = line.Split(':');
+                        if (!data.flags.ContainsKey(split_line[0])) 
+                        {
+                            missing_doors.Add(line);
+                        }
+                    }
+
+                    Random rnd = new Random();
+
+                    var rand_door = missing_doors[rnd.Next(0, missing_doors.Count() - 1)];
+                    var split_rand_door = rand_door.Split(":");
+                    textBox1.Text = String.Format("Door is found : {0} {1}", split_rand_door[0], split_rand_door[1]);
+
+                    byte[] values = data.values;
+                    bool flag = values.Length < Enum.GetNames(typeof(Vars)).Length;
+                }
+            }
+        }
+    }
+}
